840d777e7fd7c39bc0ff5bed71cd8cdeebd1b98d
Jay Yu <jay.yzhe@gmail.com>
2016-03-09 13:10:42 -0500

    search page

--- app/search/search-display.component.ts
+++ app/search/search-display.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit}       from 'angular2/core';
 import {AppFooterComponent} from '../app-footer.component';
 import {AppOfferService} from '../app-offer.service';
 import {FullOffer} from '../app-offer.service';
+import {RouteParams, Router} from 'angular2/router';
 
 @Component(
 {
@@ -14,15 +15,25 @@ import {FullOffer} from '../app-offer.service';
 
 export class SearchComponent implements OnInit {
 
-	results: FullOffer[]
+	results: FullOffer[] = []
 
 	constructor(
-		private _offerService: AppOfferService
+		private _offerService: AppOfferService,
+		private _router: Router,
+		private _routeParams: RouteParams
 	){}
 
 	ngOnInit(){
 		this._offerService.search_by_query()
 			.then(results => this.results = results);
+	}
+
+	get_query(){
+		return this._routeParams.get("query");
+	}
+
+	get_result_length(){
+		return this.results.length;
 	}
 
 }
