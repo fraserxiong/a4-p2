4b9005504b39a78eab6f15c1cef67bd86bb4799d
Fraser <fraserxiong@gmail.com>
2016-03-09 13:06:28 -0500

    add header and jquery and bootstrap

--- app/home/app-home.component.ts
+++ app/home/app-home.component.ts
@@ -3,13 +3,14 @@ import {AppGalleryComponent} from './app-gallery.component';
 import {AppOfferComponent} from './app-offer.component';
 import {AppSearchComponent} from './app-search.component';
 import {AppFooterComponent} from '../app-footer.component';
+import {AppHeaderComponent} from '../app-header.component';
 
 @Component(
 {
 	selector: "home",
 	templateUrl: "app/home/app-home.component.html",
 	styleUrls: ["css/common.css", "css/home.css"],
-	directives: [AppGalleryComponent, AppOfferComponent, AppSearchComponent, AppFooterComponent]
+	directives: [AppHeaderComponent, AppGalleryComponent, AppOfferComponent, AppSearchComponent, AppFooterComponent]
 })
 
 export class AppHomeComponent{
