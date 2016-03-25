cab06f50a736addde461ab9619b2f4867c06e569
Siyang Ye <siyang.ye@mail.utoronto.ca>
2016-03-25 19:14:39 -0400

    Use SearchInput In Search Display Component as well

--- app/search/search-display.component.ts
+++ app/search/search-display.component.ts
@@ -3,19 +3,19 @@ import { AppOfferService } from '../app-offer.service';
 import { Dish } from '../model/dish';
 import { RouteParams, Router} from 'angular2/router';
 import { DishListComponent} from '../dish/dish-list.component';
+import { SearchInputComponent } from './search-input.component';
 
 @Component(
 {
 	selector: "search",
 	templateUrl: "app/search/search-display.component.html",
 	styleUrls: ["app/search/search-display.component.css"],
-	providers: [AppOfferService],
-	directives: [DishListComponent]
+	directives: [DishListComponent, SearchInputComponent]
 })
 
 export class SearchComponent implements OnInit {
-
-	results: Dish[] = [];
+	private query: string;
+	private results: Dish[] = [];
 
 	constructor(
 		private _offerService: AppOfferService,
@@ -24,16 +24,9 @@ export class SearchComponent implements OnInit {
 	){}
 
 	ngOnInit(){
-		this._offerService.search_by_query(this.get_query())
+		this.query = this._routeParams.get('query');
+		this._offerService.search_by_query(this.query)
 			.then(results => this.results = results);
-	}
-
-	get_query(){
-		return decodeURIComponent(this._routeParams.get("query"));
-	}
-
-	get_result_length(){
-		return this.results.length;
 	}
 
 }
