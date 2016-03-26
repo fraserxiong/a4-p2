ea132a39e1085157334bd8ff8605efbb36148a42
Fraser <fraserxiong@gmail.com>
2016-03-26 14:41:38 -0400

    add express to node

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/css',express.static(path.resolve(__dirname,'css')));
+app.use('/images', express.static(path.resolve(__dirname, 'images')));
+app.use('/app', express.static(path.resolve(__dirname,'app')));
+app.use('/node_modules', express.static(path.resolve(__dirname,'node_modules')));
 
 // views is directory for all template files
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.sendFile(__dirname +'/index.html');
 });
 
 app.listen(app.get('port'), function() {
