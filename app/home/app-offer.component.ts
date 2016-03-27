9b667218afa34d87e4ce40a9ed312d8365864277
Jay Yu <jay.yzhe@gmail.com>
2016-03-27 13:20:16 -0400

    mongodb for post details and search

--- app/home/app-offer.component.ts
+++ app/home/app-offer.component.ts
@@ -14,14 +14,20 @@ import {HTTP_PROVIDERS}    from 'angular2/http';
 
 export class AppOfferComponent implements OnInit{
 	offerImages: Dish[];
+	displayImages: Dish[][];
+	max: number;
 
 	constructor(
 		private _offerService: AppOfferService,
 		private _router: Router
-	){}
+	){
+		this.max = 3;
+	}
 
 	getOfferImages(){
-		this._offerService.get_hotest_offers().then(images => this.offerImages = images);
+		this._offerService.get_hotest_offers()
+						  .then(images => {this.offerImages = images;
+							  this.displayImages = [this.offerImages.slice(0, this.max)]});
 	}
 
 	ngOnInit(){
@@ -30,5 +36,13 @@ export class AppOfferComponent implements OnInit{
 
 	get_details(post){
 		this._router.navigate(["Details", {id: post.id}]);
+	}
+
+	expand(){
+		this.max = this.max + 3;
+		this.displayImages = [];
+		for(var i = 0; i * 3 < this.max && i * 3 < 12; i++){
+			this.displayImages.push(this.offerImages.slice(i * 3, i * 3 + 3));
+		}
 	}
 }
--- mongodb_schemas/post.js
+++ mongodb_schemas/post.js
@@ -2,26 +2,49 @@ var mongoose = require('mongoose');
 
 var PostSchema = mongoose.Schema({
     id: Number,
-    url: String
+    url: String,
+    location: String,
+    description: String,
+    name: String,
+    categories: Array,
+    price: Number
 });
 
 var Post = mongoose.model('Post', PostSchema);
 
-exports.create = function(id, url){
-    var post = new Post({
-        id: id,
-        url: url
-    });
-
+exports.create = function(json){
+    var post = new Post(json);
     return post;
 }
 
 exports.find = function(id, callback){
-    Post.find({
+    Post.findOne({
         id: id
-    }, callback);
+    }).lean().exec(callback);
 }
 
 exports.all = function(callback){
-    Post.find(callback);
+    Post.find().limit(12).lean().exec(callback);
+}
+
+exports.search_by_tag = function(tags, callback){
+    var mapped = tags.map(tag => {
+        return {categories: tag};
+    });
+    console.log(mapped);
+    Post.find({
+        $or: mapped
+    }).limit(4).lean().exec(callback);
+}
+
+exports.fuzzy_search = function(query, callback){
+    var fuzzy_query = new RegExp(query, 'i');
+    Post.find({
+        $or: [
+            {categories: fuzzy_query},
+            {description: fuzzy_query},
+            {name: fuzzy_query},
+            {location: fuzzy_query}
+        ]
+    }).lean().exec(callback);
 }
