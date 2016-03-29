899f7f09683166bf4f1d19f330624b802503614b
Wen Xiao <wendyxiao0609@gmail.com>
2016-03-29 09:50:32 -0400

    still have some problem for the database, but it can use the api to post new food.

--- app/dish/dish-upload.component.ts
+++ app/dish/dish-upload.component.ts
@@ -1,17 +1,20 @@
-import {Component, Input, OnInit} from "angular2/core";
+import {Component, Input, OnInit, Inject} from "angular2/core";
 import {Dish} from "../model/dish";
 import {DishOverviewComponent} from "./dish-overview.component";
 import {UserProfileComponent} from "../user/user-profile.component";
-import {RouteParams, ROUTER_DIRECTIVES} from "angular2/router";
+import {Router,RouteParams, ROUTER_DIRECTIVES} from "angular2/router";
 import { User } from '../model/user';
 import { UserProfileService } from '../user/user-profile.service';
 import { UserAvatarComponent } from '../user/user-avatar.component';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {Headers, RequestOptions} from 'angular2/http';
 
 @Component({
 	selector: 'dish-upload',
 	templateUrl: 'app/dish/dish-upload.component.html',
 	styleUrls: ['app/dish/dish-upload.component.css'],
-	directives: [ROUTER_DIRECTIVES,DishOverviewComponent]
+	directives: [ROUTER_DIRECTIVES,DishOverviewComponent],
+	providers: [HTTP_PROVIDERS]
 })
 export class DishUploadComponent{
 	private nameHint: string = "Dish Name";
@@ -26,7 +29,41 @@ export class DishUploadComponent{
 
 	constructor(
 		private _profileService: UserProfileService,
-		private _routeParams: RouteParams){}
+		private _routeParams: RouteParams,
+		private http: Http,
+		@Inject(Router) private _router: Router){}
+
+
+	onUpload( name ,location,description,categories, price, url){
+		var cat = categories.split(",");
+		var new_dish={
+		name : name,
+		locaton :location,
+		description : description,
+		categories : cat,
+		price : price,
+		url : url
+		};
+
+
+		var data = JSON.stringify(new_dish);
+		this.http.post('/posts/create', data)
+			.toPromise()
+			.then(res => res.json().data)
+			.catch(this.handleError);
+		this.onAuthenticationPass();
+	}
+
+	onAuthenticationPass() {
+		this._router.navigate(['UserMyDish',{id: this.curId}]);
+	}
+
+	private handleError (error: any) {
+	  // in a real world app, we may send the error to some remote logging infrastructure
+	  // instead of just logging it to the console
+	  console.error(error);
+	  return Promise.reject(error.message || error.json().error || 'Server error');
+	}
 
 	ngOnInit(){
 		let id: number = + this._routeParams.get('id');
