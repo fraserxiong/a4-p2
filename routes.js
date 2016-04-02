a5ad0e320fb8dee3545ec40e4b0fa241d6496da4
Fraser <fraserxiong@gmail.com>
2016-04-02 17:07:51 -0400

    del bug fix

--- routes.js
+++ routes.js
@@ -59,10 +59,12 @@ exports = module.exports = function (app, passport) {
   // User info
   app.all('/api/account*', apiEnsureAuthenticated);
   app.all('/api/account*', apiEnsureAccount);
-  app.get('/api/account/user', require('./api/user/rest').get_user);
   app.get('/api/account/user/settings', require('./api/user/rest').get_user_settings);
+  app.get('/api/account/user/friend', require("./api/user/rest").get_friend_list);
   app.put('/api/account/user/add_friend/:friend_id/', require('./api/user/rest').add_friend);
+  app.post('/api/account/user/del_friend/:friend_id/', require("./api/user/rest").del_friend);
   app.get('/api/account/user/:user_id/', require("./api/user/rest").get_basic_user_info);
+  app.get('/api/account/user/', require('./api/user/rest').get_user);
   app.get('/api/account/', require("./api/user/rest").search_user);
 
   //sign up
