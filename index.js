d4fb3902b0e82ef7febd21caab03598608d82e8e
Jay Yu <jay.yzhe@gmail.com>
2016-04-03 21:50:26 -0400

    file upload

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ app.use(passport.session());
 helmet(app);
 
 //response locals
-
 app.use(function(req, res, next) {
   // res.cookie('_csrfToken', req.csrfToken());
   res.locals.user = {};
@@ -70,6 +69,31 @@ app.use(function(req, res, next) {
   next();
 });
 
+// upload stuff
+var multer  = require('multer');
+var upload = multer({ dest: 'upload/'});
+var fs = require('fs');
+
+var type = upload.single('photo');
+
+app.post('/upload', type, function (req,res) {
+
+  /** When using the "single"
+      data come in "req.file" regardless of the attribute "name". **/
+  var tmp_path = req.file.path;
+
+  /** The original name of the uploaded file
+      stored in the variable "originalname". **/
+  var target_path = 'uploads/' + req.file.originalname;
+
+  /** A better way to copy the uploaded file. **/
+  var src = fs.createReadStream(tmp_path);
+  var dest = fs.createWriteStream(target_path);
+  src.pipe(dest);
+  src.on('end', function() { res.render('complete'); });
+  src.on('error', function(err) { res.render('error'); });
+
+});
 
 //global locals
 app.locals.projectName = app.config.projectName;
