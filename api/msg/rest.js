f753221ab1fbbcd40309f9a73592c700797dbfb9
Fraser <fraserxiong@gmail.com>
2016-04-05 00:50:36 -0400

    accept and decline API finished

--- api/msg/rest.js
+++ api/msg/rest.js
@@ -39,43 +39,47 @@ exports.accept_friend_msg = function(req, res, next){
   .exec(function (err, msg_obj) {
     console.log(msg_obj);
     if (err) throw err;
-    var isInArray = msg_obj.friendMsg.some(function (friend) {
-      if(friend.req_acc_id.equals(req.params.acc_id)){
-        msg_obj.friendMsg.pull(friend);
-        msg_obj.save();
-        return true;
-      }else{
-        return false;
-      }
-    });
-    if(msg_obj.friendMsg.length  == 0 || !isInArray){
-      res.status(403).send("Friend request not exist");
-    }else{
-      req.app.db.models.Account.findById(req.params.acc_id)
-      .populate('user.id')
-      .exec(function (err, friend_ref) {
-        if (err) throw err;
-        req.app.db.models.Friend.findOne({user:req.user.roles.account.id, })
-        .populate('user')
-        .exec(function (err, friend_obj) {
-          if (err) throw err;
-          if(friend_ref && friend_obj){var isInArray = friend_obj.friend.some(function (friend) {
-            return friend.equals(friend_ref._id);
-          });
-          if(friend_obj.friend.length > 0 && isInArray){
-            res.status(403).send("Already added");
-          }else{
-            friend_obj.friend.push(friend_ref);
-            friend_obj.save();
-            // console.log(friend_obj);
-            msg_api.friend_request(req.app, friend_ref, friend_obj.user);
-            res.status(200).send("Add friend success");
-          }
+    if(msg_obj){
+      var isInArray = msg_obj.friendMsg.some(function (friend) {
+        if(friend.req_acc_id.equals(req.params.acc_id)){
+          msg_obj.friendMsg.pull(friend);
+          msg_obj.save();
+          return true;
         }else{
-          res.status(403).send("Already added");
+          return false;
         }
       });
-    });
+      if(msg_obj.friendMsg.length  == 0 || !isInArray){
+        res.status(403).send("Friend request not exist");
+      }else{
+        req.app.db.models.Account.findById(req.params.acc_id)
+        .populate('user.id')
+        .exec(function (err, friend_ref) {
+          if (err) throw err;
+          req.app.db.models.Friend.findOne({user:req.user.roles.account.id, })
+          .populate('user')
+          .exec(function (err, friend_obj) {
+            if (err) throw err;
+            if(friend_ref && friend_obj){var isInArray = friend_obj.friend.some(function (friend) {
+              return friend.equals(friend_ref._id);
+            });
+            if(friend_obj.friend.length > 0 && isInArray){
+              res.status(403).send("Already added");
+            }else{
+              friend_obj.friend.push(friend_ref);
+              friend_obj.save();
+              // console.log(friend_obj);
+              msg_api.friend_request(req.app, friend_ref, friend_obj.user);
+              res.status(200).send("Add friend success");
+            }
+          }else{
+            res.status(403).send("Already added");
+          }
+        });
+      });
+      }
+    }else{
+      res.status(403).send("User error");
     }
   });
 };
@@ -86,7 +90,24 @@ exports.decline_friend_msg = function(req, res, next){
   .exec(function (err, msg_obj) {
     console.log(msg_obj);
     if (err) throw err;
-    res.send(JSON.stringify({}));
+    if(msg_obj.friendMsg){
+      var isInArray = msg_obj.friendMsg.some(function (friend) {
+        if(friend.req_acc_id.equals(req.params.acc_id)){
+          msg_obj.friendMsg.pull(friend);
+          msg_obj.save();
+          return true;
+        }else{
+          return false;
+        }
+      });
+      if(isInArray){
+        res.status(200).send("Accept friend success");
+      }else{
+        res.status(403).send("Request not exist");
+      }
+    }else{
+      res.status(403).send("User error");
+    }
   });
 };
 
