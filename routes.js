46b1e1a325ff8c9aaa4150988e1d0760644ec1ad
Wen Xiao <wendyxiao0609@gmail.com>
2016-04-06 20:18:17 -0400

    update some url of rest

--- routes.js
+++ routes.js
@@ -62,8 +62,8 @@ exports = module.exports = function (app, passport) {
   app.all('/api/account*', apiEnsureAccount);
   app.get('/api/account/user/settings', require('./api/user/rest').get_user_settings);
   app.get('/api/account/user/friend/', require("./api/user/rest").get_friend_list);
-  app.put('/api/account/user/add_friend/:friend_id/', require('./api/user/rest').add_friend);
-  app.delete('/api/account/user/del_friend/:friend_id/', require("./api/user/rest").del_friend);
+  app.put('/api/account/user/:friend_id/', require('./api/user/rest').add_friend);
+  app.delete('/api/account/user/:friend_id/', require("./api/user/rest").del_friend);
   app.get('/api/account/user', require('./api/user/rest').get_user);
   app.get('/api/account/', require("./api/user/rest").search_user);
 
